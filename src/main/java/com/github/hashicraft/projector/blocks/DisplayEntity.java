8b184a887590b350652cdbf12cda2a2a8928d4f0
Erik Veld <=>
2021-09-14 13:57:36 +0200

    Cleanup

--- src/main/java/com/github/hashicraft/projector/blocks/DisplayEntity.java
+++ src/main/java/com/github/hashicraft/projector/blocks/DisplayEntity.java
@@ -120,6 +120,8 @@ public class DisplayEntity extends StatefulBlockEntity {
         startBlockDirection = Direction.WEST;
         widthBlockDirection = Direction.EAST;
         break;
+      default:
+        break;
     }
 
     // Check if the block is redstone powered
